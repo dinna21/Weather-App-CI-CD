6b7f507157157b37ba5861ae75d0f5e3efdc021f
dinna21 <ransikadinura@gmail.com>
2025-04-10 10:18:47 +0530

    Add the base python code for my project

---
+++ python-code.py
@@ -0,0 +1,13 @@
+import requests
+
+API_KEY = "cd11d6c2f2870e35a11447f30fc4c0ff"
+CITY = input("Enter city name")
+
+url=f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+
+response = requests.get(url)
+data = response.json()
+
+print(f"Weather in {CITY}: {data['weather'][0]['description']}")
+print(f"Temperature: {data[main]['temp']}°C")
+
